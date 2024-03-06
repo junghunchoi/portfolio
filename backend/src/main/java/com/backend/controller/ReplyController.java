1c1e566c15d7830e85d493626963c2395701788a
junghunchoi <azzudi@gmail.com>
2024-03-06 22:58:35 +0900

    [2024.03.06] 클라이언트 커밋

--- backend/src/main/java/com/backend/controller/ReplyController.java
+++ backend/src/main/java/com/backend/controller/ReplyController.java
@@ -3,11 +3,14 @@ package com.backend.controller;
 import com.backend.dto.PageRequestDTO;
 import com.backend.dto.PageResponseDTO;
 import com.backend.dto.ReplyDTO;
+import com.backend.dto.ResultDTO;
 import com.backend.service.ReplyService;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
@@ -26,7 +29,7 @@ public class ReplyController {
 
 	@ApiOperation(value = "Replies POST", notes = "POST 방식으로 댓글등록")
 	@PostMapping(value = "/", consumes = MediaType.APPLICATION_JSON_VALUE)
-	public Map<String, Long> register(@Valid @RequestBody ReplyDTO replyDTO, BindingResult bindingResult) throws BindException {
+	public ResponseEntity<?> register(@Valid @RequestBody ReplyDTO replyDTO, BindingResult bindingResult) throws BindException {
 
 		if (bindingResult.hasErrors()) {
 			throw new BindException(bindingResult);
@@ -37,7 +40,7 @@ public class ReplyController {
 
 		resultMap.put("rno", rno);
 
-		return resultMap;
+		return ResponseEntity.ok(rno);
 	}
 
 	@ApiOperation(value="Replies of Board", notes = "GET 방식으로 특정 게시물 댓글목록")
@@ -50,28 +53,10 @@ public class ReplyController {
 
 	}
 
-//	@ApiOperation(value = "Read Only", notes = "GET 방식으로 특정 댓글조회")
-//	@GetMapping("/{rno}")
-//	public ReplyDTO getReplyDTO(@PathVariable("rno") Long rno) {
-//
-//		ReplyDTO replyDTO = replyService.read(rno);
-//
-//		return replyDTO;
-//	}
-
-	@DeleteMapping("/{rno}")
-	public Map<String,Long> remove (@PathVariable("rno") Long rno) {
-		replyService.remove(rno);
-
-		Map<String, Long> resultMap = new HashMap<>();
-		resultMap.put("rno", rno);
-
-		return resultMap;
-	}
 
 	@ApiOperation(value = "Modify Reply", notes = "PUT 방식으로 특정 댓글 수정")
-	@PutMapping(value = "/{rno}", consumes = MediaType.APPLICATION_JSON_VALUE )
-	public Map<String,Long> remove( @PathVariable("rno") Long rno, @RequestBody ReplyDTO replyDTO ){
+	@DeleteMapping(value = "/{rno}", consumes = MediaType.APPLICATION_JSON_VALUE )
+	public ResponseEntity<ResultDTO<String>> remove( @PathVariable("rno") Long rno, @RequestBody ReplyDTO replyDTO ){
 
 		replyDTO.setRno(rno); //번호를 일치시킴
 
@@ -81,7 +66,7 @@ public class ReplyController {
 
 		resultMap.put("rno", rno);
 
-		return resultMap;
+		return ResponseEntity.ok(ResultDTO.res(HttpStatus.OK, HttpStatus.OK.toString(), "Delete Reply SuccessFully"));
 	}
 
 }
