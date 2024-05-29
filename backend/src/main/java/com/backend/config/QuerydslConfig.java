4a3cb069e729ab71e0337d37d257f3a470ad91a7
junghunchoi <azzudi@gmail.com>
2024-05-29 23:50:06 +0900

    [2024.05.22] 리드미 수정

--- backend/src/main/java/com/backend/config/QuerydslConfig.java
+++ backend/src/main/java/com/backend/config/QuerydslConfig.java
@@ -4,9 +4,7 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 
 @Configuration
 @RequiredArgsConstructor
