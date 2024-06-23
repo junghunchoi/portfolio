9c846bfe0dfcc1c6aba963572f015488832947b3
junghunchoi <azzudi@gmail.com>
2024-06-23 20:47:38 +0900

    [2024.06.23] 1. swagger ui 정리

---
+++ backend/src/main/java/com/backend/dto/PageRequestDTO.java
@@ -0,0 +1,57 @@
+package com.backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+/**
+ * 페이지 요총 DTO 클래스.
+ * 페이징 처리된 데이터와 검색 정보등의 정보를 포함합니다.
+ *
+ */
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Log4j2
+public class PageRequestDTO {
+
+	@Builder.Default
+	private int page = 1;
+
+	@Builder.Default
+	private int size = 10;
+
+	private String type; // 검색의 종류 ex)t,c,w ...
+
+	private String keyword;
+
+	private String order;
+
+	private String sort;
+
+
+	//검색 조건을 문자열에서 배열로 반환
+	public String[] getTypes() {
+		if (type == null || type.isEmpty()) {
+			return null;
+		}
+		return type.split("");
+	}
+
+	//사용하는 pageable 타입을 반환하는 기능도 필요하다.
+	public Pageable getPageable(String...props) {
+		if (this.sort.equals("asc") ) {
+			return PageRequest.of(this.page - 1, this.size, Sort.by(props).ascending());
+		}else {
+			return PageRequest.of(this.page - 1, this.size, Sort.by(props).descending());
+		}
+	}
+
+
+}
