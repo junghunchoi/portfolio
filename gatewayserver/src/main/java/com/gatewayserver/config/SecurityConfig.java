a57049099b2e08e81cfeacb6e9ab08bbbe20f481
azzudi <azzudi@naver.com>
2024-06-25 16:24:14 +0900

    [2024.06.25] 1. msa 변환작업 진행. 2. config 종료 3. eureka 셋업마무리 4. board 어플리케이션 등록마무리 5. gateway 적용

---
+++ gatewayserver/src/main/java/com/gatewayserver/config/SecurityConfig.java
@@ -0,0 +1,52 @@
+package com.gatewayserver.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverter;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+	/**
+	 *  .pathMatchers("/eazybank/accounts/**").authenticated() 해당 url의 모든 요청은 인증되어야한다.
+	 *  .pathMatchers("/eazybank/accounts/**").hasRole("ACCOUNTS") 해당 권한을 가진 사람만 접근할 수 있다.
+	 */
+
+	@Bean
+	public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity) {
+		serverHttpSecurity.authorizeExchange(exchanges -> exchanges.pathMatchers(HttpMethod.GET)
+		                                                           .permitAll() // anyExchange().authenticated() 모든 요청은 인증되어야한다.
+		                                                           .pathMatchers(
+			                                                           "/eazybank/accounts/**")
+		                                                           .hasRole("ACCOUNTS")
+		                                                           .pathMatchers(
+			                                                           "/eazybank/cards/**")
+		                                                           .hasRole("CARDS")
+		                                                           .pathMatchers(
+			                                                           "/eazybank/loans/**")
+		                                                           .hasRole("LOANS")).oauth2ResourceServer(oAuth2ResourceServerSpec -> oAuth2ResourceServerSpec.jwt(
+			jwtSpec -> jwtSpec.jwtAuthenticationConverter(gratedAuthoritiesExtractor())));
+
+		serverHttpSecurity.csrf(csrfSpec -> csrfSpec.disable());
+
+		return serverHttpSecurity.build();
+	}
+
+	private Converter<Jwt, Mono<AbstractAuthenticationToken>> gratedAuthoritiesExtractor() {
+		JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+
+		jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRoleConverter());
+
+		return new ReactiveJwtAuthenticationConverter();
+	}
+}
