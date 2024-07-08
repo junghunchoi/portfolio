07bed888c27f8194e56c15917de89ec3de8bce93
DESKTOP-GEILVKG\eMoney <azzudi@emoney.co.kr>
2024-07-08 13:22:30 +0900

    clear git cache

---
+++ securityserver/src/main/java/com/securityserver/exception/SecurityAdvice.java
@@ -0,0 +1,29 @@
+package com.securityserver.exception;
+
+import com.securityserver.common.dto.ErrorResponse;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+@Log4j2
+public class SecurityAdvice {
+
+    @ExceptionHandler(Exception.class)
+    protected final ResponseEntity<ErrorResponse> handleAllExceptions(Exception e, WebRequest webRequest) {
+        log.error("예기치 않은 오류가 발생했습니다: {}", e.getMessage());
+        ErrorResponse errorResponse = new ErrorResponse(
+                webRequest.getDescription(false),
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                e.getMessage(),
+                LocalDateTime.now()
+        );
+        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+}
