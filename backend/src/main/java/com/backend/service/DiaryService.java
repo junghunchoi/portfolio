93a4b452a0f6955684d0352881c7c16b775e06ad
DESKTOP-GEILVKG\eMoney <azzudi@emoney.co.kr>
2024-07-31 17:29:56 +0900

    [2024.07.31] 오늘 jar 다말고 서비스 로직 추가해서 반드시 실행

---
+++ backend/src/main/java/com/backend/service/DiaryService.java
@@ -0,0 +1,18 @@
+package com.backend.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Log4j2
+public class DiaryService {
+
+    //TODO 자전거 주행기록
+    //TODO 책 읽은 양
+    //TODO 공부시간
+    //TODO 포스트 갯수
+    //TODO
+
+}
